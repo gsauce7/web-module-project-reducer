000774a7cbef2e27a78fa515736f42351ca28dec
Gabriel Henton <gabehenton@gmail.com>
2021-04-21 20:50:16 -0700

    MVP

--- src/App.js
+++ src/App.js
@@ -1,62 +1,166 @@
-import React from 'react';
-
+import React, { useReducer } from 'react';
 import './App.css';
 
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
+import reducer, { initialState, calculateResult } from './reducers/index';
+
+import {
+  addOne,
+  memoryClear,
+  applyNumber,
+  changeOperator,
+  clearDisplay,
+  memoryAdd,
+  memoryReturn,
+} from './actions/index';
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const clicker = (num) => {
+    dispatch(applyNumber(num));
+  };
+
+  const changeOperation = (operator) => {
+    dispatch(changeOperator(operator));
+  };
 
   return (
-    <div className="App">
-      <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#"><img width="40px" src="./Lambda-Logo-Red.png"/> Lambda Reducer Challenge</a>
+    <div className='App'>
+      <nav className='navbar navbar-dark bg-dark'>
+        <a className='navbar-brand' href='#'>
+          <img width='40px' src='./Lambda-Logo-Red.png' /> Lambda Reducer
+          Challenge
+        </a>
       </nav>
 
-      <div className = "container row mt-5">
-        <div className="col-md-12 d-flex justify-content-center">
-          <form name="Cal">
-            
-            <TotalDisplay value={0}/>
-            <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
-            </div>
-            
-            <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+      <div className='container row mt-5'>
+        <div className='col-md-12 d-flex justify-content-center'>
+          <form name='Cal'>
+            <TotalDisplay value={state.total} />
+            <div className='row details'>
+              <span id='operation'>
+                <b>Operation:</b> {state.operation}
+              </span>
+              <span id='memory'>
+                <b>Memory:</b> {state.memory}
+              </span>
             </div>
 
-            <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+            <div className='row'>
+              <CalcButton
+                onClick={() => {
+                  dispatch(memoryAdd());
+                }}
+                value={'M+'}
+              />
+              <CalcButton
+                onClick={() => {
+                  dispatch(memoryReturn());
+                }}
+                value={'MR'}
+              />
+              <CalcButton
+                onClick={() => {
+                  dispatch(memoryClear());
+                }}
+                value={'MC'}
+              />
             </div>
 
-            <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+            <div className='row'>
+              <CalcButton
+                //onClick={() => dispatch(addOne(1))} yes this adds 1 if uncommented
+                onClick={() => {
+                  clicker(1);
+                }}
+                value={1}
+              />
+              <CalcButton
+                onClick={() => {
+                  clicker(2);
+                }}
+                value={2}
+              />
+              <CalcButton
+                onClick={() => {
+                  clicker(3);
+                }}
+                value={3}
+              />
             </div>
 
-            <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+            <div className='row'>
+              <CalcButton
+                onClick={() => {
+                  clicker(4);
+                }}
+                value={4}
+              />
+              <CalcButton
+                onClick={() => {
+                  clicker(5);
+                }}
+                value={5}
+              />
+              <CalcButton
+                onClick={() => {
+                  clicker(6);
+                }}
+                value={6}
+              />
             </div>
 
-            <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+            <div className='row'>
+              <CalcButton
+                onClick={() => {
+                  clicker(7);
+                }}
+                value={7}
+              />
+              <CalcButton
+                onClick={() => {
+                  clicker(8);
+                }}
+                value={8}
+              />
+              <CalcButton
+                onClick={() => {
+                  clicker(9);
+                }}
+                value={9}
+              />
             </div>
 
-            <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+            <div className='row'>
+              <CalcButton
+                onClick={() => {
+                  changeOperation('+');
+                }}
+                value={'+'}
+              />
+              <CalcButton
+                onClick={() => {
+                  changeOperation('*');
+                }}
+                value={'*'}
+              />
+              <CalcButton
+                onClick={() => {
+                  changeOperation('-');
+                }}
+                value={'-'}
+              />
             </div>
 
+            <div className='row ce_button'>
+              <CalcButton
+                onClick={() => {
+                  dispatch(clearDisplay());
+                }}
+                value={'CE'}
+              />
+            </div>
           </form>
         </div>
       </div>
